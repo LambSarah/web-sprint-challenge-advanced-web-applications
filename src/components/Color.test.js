2607ebb604f10f0dcb196f060c248d4d065616a6
Sarah Guidry <sarahbethguidry@gmail.com>
2021-06-14 01:40:32 -0500

    couldnt figure out problem and had no help

--- src/components/Color.test.js
+++ src/components/Color.test.js
@@ -4,8 +4,10 @@ import userEvent from "@testing-library/user-event";
 import Color from './Color';
 
 test("Renders without errors with blank color passed into component", () => {
+	//render(<Color color={null} />)
+
 });
-  
+
 test("Renders the color passed into component", () => {
 });
 
@@ -13,5 +15,5 @@ test("Executes handleDelete and toggleEdit property when the 'x' icon is clicked
 });
 
 test("Executes setEditColor and toggleEdit property when color div is clicked", () => {
-    
+
 });
--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import { render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import ColorList from './ColorList';
 
+const emptyColorList = []
+
+const testColors = [
+	{
+		color: "aliceblue",
+		code: {
+			hex: "#f0f8ff",
+		},
+		id: 1,
+	},
+	{
+		color: "limegreen",
+		code: {
+			hex: "#99ddbc",
+		},
+		id: 2,
+	},
+	{
+		color: "aqua",
+		code: {
+			hex: "#00ffff",
+		},
+		id: 3,
+	},
+]
+
 test("Renders an empty list of colors without errors", () => {
+	render(<ColorList colors={emptyColorList} />)
 });
 
 test("Renders a list of colors without errors", () => {
+	render(<ColorList colors={testColors} />)
 });
 
 test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
+	render(<ColorList colors={testColors} editing={true} />)
+
 });
